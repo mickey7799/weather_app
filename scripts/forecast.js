6ad614a10cd8695399758446fa61ed258bb4dcc7
Mickey <vickeylee.w@gmail.com>
2020-08-16 23:45:00 +1000

    add function getCity

---
+++ scripts/forecast.js
@@ -0,0 +1,16 @@
+const key = 'rvzdWxHPGrGO95fHjJOiek09Ao3P70K7';
+
+// get city information
+const getCity = async city => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('manchester')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
